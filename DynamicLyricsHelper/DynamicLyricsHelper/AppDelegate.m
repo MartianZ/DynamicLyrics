7cb884fc0725112c1f8f20b6eee877f6f81198ee
MartianZ <fzyadmin@gmail.com>
2012-05-19 14:34:56 +0800

    update DynamicLyricsHelper

--- DynamicLyricsHelper/DynamicLyricsHelper/AppDelegate.m
+++ DynamicLyricsHelper/DynamicLyricsHelper/AppDelegate.m
@@ -7,12 +7,73 @@
 //
 
 #import "AppDelegate.h"
+#include "iTunes.h"
+/*
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define DEFAULT_INTERVAL 2
+#define DEFAULT_LOGFILE "daemon.txt"*/
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    
+    iTunesApplication *iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+    while (1) {
+        while ([iTunes isRunning]) {
+            sleep(2);
+        }
+        //ITUNES EXIT
+        NSLog(@"%@",@"ITUNES EXIT");
+        
+        
+        
+        
+        
+        
+        while (![iTunes isRunning]) {
+            sleep(2);
+        }
+        //ITUNES START
+        NSLog(@"%@",@"ITUNES START");
+        
+        
+        
+    }
+
     exit(0);
+    
+    
+    /*pid_t pid, sid;
+    pid = fork();
+
+    umask(0);
+    
+    sid = setsid();
+    
+    if (sid < 0) {
+		exit(EXIT_FAILURE);
+    }
+    
+
+    //SON THREAD
+    printf("sid=%d\n",sid);
+  	if ((chdir("/Users/Martian/Desktop/")) < 0) {
+		printf("chdir error\n");
+		exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+
+        
+        
+    }
+    exit(0);*/
 }
 
 @end
