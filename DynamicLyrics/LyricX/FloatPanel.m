d0efa1e7831540cf343758533100a453db76fe72
MartianZ <fzyadmin@gmail.com>
2012-06-17 21:43:16 +0800

    Revert "fix memory leaks"
    This reverts commit ab5cfdd6800d5d77a05987195548ab8dbec2b0c3.

--- DynamicLyrics/LyricX/FloatPanel.m
+++ DynamicLyrics/LyricX/FloatPanel.m
@@ -16,7 +16,7 @@
     if (self) {
         //self.backgroundColor = [NSColor colorWithCalibratedWhite:0 alpha:0.3];
         self.backgroundColor = NSColor.clearColor;
-        self.level = NSStatusWindowLevel; //CGShieldingWindowLevel();
+        self.level = NSFloatingWindowLevel; //CGShieldingWindowLevel();
         self.opaque = NO;
         self.hasShadow = NO;
         self.hidesOnDeactivate = NO;
